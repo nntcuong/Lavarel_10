4c9306a5b1da08abd2a547e4a06538f34cab81a9
nntcuong <thecuongyt3htk@gmail.com>
2024-04-11 20:47:07 +0700

    fdf

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,32 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function index(){
-        $blogs=[
-            [
-                'title' =>'Title one',
-                'body' =>'This is a body text',
-                'status' =>1
-            ],
-            [
-                'title' =>'Title two',
-                'body' =>'This is a body text',
-                'status' =>0
-            ],
-            [
-                'title' =>'Title three',
-                'body' =>'This is a body text',
-                'status' =>1
-            ],
-            [
-                'title' =>'Title four',
-                'body' =>'This is a body text',
-                'status' =>0
-            ],
-        ];
+        // $blogs=[
+        //     [
+        //         'title' =>'Title one',
+        //         'body' =>'This is a body text',
+        //         'status' =>1
+        //     ],
+        //     [
+        //         'title' =>'Title two',
+        //         'body' =>'This is a body text',
+        //         'status' =>0
+        //     ],
+        //     [
+        //         'title' =>'Title three',
+        //         'body' =>'This is a body text',
+        //         'status' =>1
+        //     ],
+        //     [
+        //         'title' =>'Title four',
+        //         'body' =>'This is a body text',
+        //         'status' =>0
+        //     ],
+        // ];
+        return DB::table("posts")->find(7);
         return view('home',compact('blogs'));
     }
 }
--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
@@ -11,14 +12,14 @@ class LoginController extends Controller
         return view('login');
     }
 
-    public function handleLogin(Request $request){
+    public function handleLogin(LoginRequest $request){
 
-        $request->validate([
-            'name'=>'required',
-        'email'=>'required',
-        'password'=>'required',
-        ]
-    );
+    //     $request->validate(,[
+    //         'name.required'=>'The user name field is required',
+    //         'name.alpha'=>'The user name field is',
+    //         'email.email'=>'The user email field is required',
+    //     ]
+    // );
         return $request;
     }
 }
